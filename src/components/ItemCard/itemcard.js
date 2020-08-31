c82f24d1983ce8692fa258cb65ca785e11703b6c
olcaneristi <ogulcaneristi123@gmail.com>
2020-08-31 03:07:33 +0300

    react modal eklendi. film bilgileri pop-up'a aktarildi

--- src/components/ItemCard/itemcard.js
+++ src/components/ItemCard/itemcard.js
@@ -1,23 +1,43 @@
-import React, { forwardRef } from "react"
+import React, { forwardRef, useState } from "react"
+import Modal from "react-modal"
 
 const base_url = "https://image.tmdb.org/t/p/original"
 
+Modal.setAppElement("#root")
+
 const ItemCard = forwardRef(({ movie }, ref) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false)
+
   return (
     <div className="itemcard">
       <div ref={ref} className="itemcard-container">
-        <img alt="view" src={`${base_url}${movie.backdrop_path}`} />
-        <h3>{movie.title}</h3>
+        <button onClick={() => setModalIsOpen(true)}>
+          <img
+            alt="view"
+            loading="lazy"
+            src={`${base_url}${movie.backdrop_path}`}
+          />
+        </button>
 
-        <p>
-          <span>{movie.overview}</span>
-        </p>
-        <p>
-          Release Date: <span>{movie.release_date}</span>
-        </p>
-        <p>
-          Rating: <span>{movie.vote_average}</span>
-        </p>
+        <Modal
+          isOpen={modalIsOpen}
+          portalClassName="modal"
+          onRequestClose={() => setModalIsOpen(false)}
+        >
+          <h3>{movie.title}</h3>
+          <p>
+            <span>{movie.overview}</span>
+          </p>
+          <p>
+            Release Date: <span>{movie.release_date}</span>
+          </p>
+          <p>
+            Rating: <span>{movie.vote_average}</span>
+          </p>
+          <p>
+            Likes: <span>{movie.vote_count}&nbsp;people like this. </span>
+          </p>
+        </Modal>
       </div>
     </div>
   )
