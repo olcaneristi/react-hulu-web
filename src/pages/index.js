4141895f3dcf26e50413140371a453f10881d980
olcaneristi <ogulcaneristi123@gmail.com>
2020-09-30 01:00:01 +0300

    sayfalari toplu export etmek icin index eklendi

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Collections from "./collections"
+import Home from "./home"
+import Profile from "./profile"
+import Trending from "./trending"
+import Verified from "./verified"
+
+export { Collections, Home, Profile, Trending, Verified }
