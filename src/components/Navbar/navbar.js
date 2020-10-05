dd1ede7fb72cf691487221bce7255d032ba09be6
olcaneristi <ogulcaneristi123@gmail.com>
2020-10-05 15:40:19 +0300

    kod tekrarindan kurtulup kategorileri loop'a aldik

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -5,18 +5,13 @@ function Navbar({ setOption }) {
   return (
     <section className="navbar">
       <div className="nav-container">
-        <h2 onClick={() => setOption(requests.fetchTrending)}>Trending</h2>
-        <h2 onClick={() => setOption(requests.fetchTopRated)}>Top Rated</h2>
-        <h2 onClick={() => setOption(requests.fetchTheBest)}>All-Time Best</h2>
-        <h2 onClick={() => setOption(requests.fetchUpcoming)}>Upcoming</h2>
-        <h2 onClick={() => setOption(requests.fetchAction)}>Action</h2>
-        <h2 onClick={() => setOption(requests.fetchCrime)}>Crime</h2>
-        <h2 onClick={() => setOption(requests.fetchComedy)}>Comedy</h2>
-        <h2 onClick={() => setOption(requests.fetchHistory)}>History</h2>
-        <h2 onClick={() => setOption(requests.fetchHorror)}>Horror</h2>
-        <h2 onClick={() => setOption(requests.fetchMystery)}>Mystery</h2>
-        <h2 onClick={() => setOption(requests.fetchRomance)}>Romance</h2>
-        <h2 onClick={() => setOption(requests.fetchThriller)}>Thriller</h2>
+        {requests.map((item, index) => {
+          return (
+            <h2 key={index} onClick={() => setOption(item.category)}>
+              {item.title}
+            </h2>
+          )
+        })}
       </div>
     </section>
   )
