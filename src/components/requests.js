60a737f0f505b6964fb4924bd4730b3bcb8ae6f8
olcaneristi <ogulcaneristi123@gmail.com>
2020-10-05 15:36:39 +0300

    kategoriler array haline getirildi

--- src/components/requests.js
+++ src/components/requests.js
@@ -1,16 +1,52 @@
 const API_KEY = "ccb0a8566b23ab43471cda53fed3d9e7"
 
-export default {
-  fetchTheBest: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=vote_count.desc`,
-  fetchTrending: `/trending/all/day?api_key=${API_KEY}&language=tr-TR`,
-  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=tr-TR`,
-  fetchUpcoming: `/movie/upcoming?api_key=${API_KEY}&language=tr-TR`,
-  fetchAction: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=28`,
-  fetchCrime: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=80`,
-  fetchComedy: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=35`,
-  fetchHistory: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=36`,
-  fetchHorror: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=27`,
-  fetchMystery: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=9648`,
-  fetchRomance: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=10749`,
-  fetchThriller: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=53`
-}
+export default [
+  {
+    category: `/trending/all/day?api_key=${API_KEY}&language=tr-TR`,
+    title: "Trending"
+  },
+  {
+    category: `/movie/top_rated?api_key=${API_KEY}&language=tr-TR`,
+    title: "Top Rated"
+  },
+  {
+    category: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=vote_count.desc`,
+    title: "All-Time Best"
+  },
+  {
+    category: `/movie/upcoming?api_key=${API_KEY}&language=tr-TR`,
+    title: "Upcoming"
+  },
+  {
+    category: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=28`,
+    title: "Action"
+  },
+  {
+    category: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=80`,
+    title: "Crime"
+  },
+  {
+    category: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=35`,
+    title: "Comedy"
+  },
+  {
+    category: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=36`,
+    title: "History"
+  },
+  {
+    category: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=27`,
+    title: "Horror"
+  },
+  {
+    category: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=9648`,
+    title: "Mystery"
+  },
+  {
+    category: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=10749`,
+    title: "Romance"
+  },
+  {
+    category: `/discover/movie?api_key=${API_KEY}&language=tr-TR&sort_by=popularity.desc&with_genres=53`,
+    title: "Thriller"
+  }
+]
